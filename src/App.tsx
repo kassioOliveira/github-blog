96cc9f2b67ec8b4628b7a9435e0d44a7055114c2
kassio oliveira <kassiooliveira875@gmail.com>
2023-05-08 15:19:36 -0300

    Global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './style/global'
+import { defaultTheme } from './style/themes/default'
+
 export function App() {
-  return <h1>ola</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>ola</h1>
+      <p>akksoosososossosososososo</p>
+      <p>akksoosososossosososososo</p>
+    </ThemeProvider>
+  )
 }
---
+++ src/style/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+        background-color: ${(props) => props.theme['base-background']};
+        color: ${(props) => props.theme['base-text']};
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea,button {
+        font: 400 1rem Nunito, sans-serif;
+        
+    }
+
+`
