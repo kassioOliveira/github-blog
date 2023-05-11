f6c6d6cfad1c0767808ba6c1827a6e169200c455
kassio oliveira <kassiooliveira875@gmail.com>
2023-05-11 14:54:15 -0300

    new Route

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { Home } from './pages/Home'
 import { Article } from './pages/Article'
+import { NotFound } from './pages/NotFound'
 
 export function Router() {
   return (
@@ -9,6 +10,8 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/article/:id" element={<Article />} />
+        <Route path="/notfound" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   )
